4e81dddb4fa19a479f77e6692fedde137195948d
KrystianZmuda <121794951+KrystianZmuda@users.noreply.github.com>
2025-11-07 14:40:10 +0100

    delete log

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     while True:
